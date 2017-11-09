b09b802319c81a8c24c7177645f117234cd0ad2f
Liran <liranbh@gmail.com>
2017-11-09 16:08:41 +0200

    final

--- src/app/simp-mod/simp-mod.module.ts
+++ src/app/simp-mod/simp-mod.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {ModuleWithProviders, NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MathService} from "../math.service";
 import {SimpComponent} from "../simp/simp.component";
@@ -8,7 +8,14 @@ import {SimpComponent} from "../simp/simp.component";
     CommonModule
   ],
   declarations: [SimpComponent],
-  providers: [MathService],
   exports: [SimpComponent]
 })
-export class SimpModModule { }
+export class SimpModModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SimpModModule,
+      providers: [MathService]
+    }
+  }
+
+}
